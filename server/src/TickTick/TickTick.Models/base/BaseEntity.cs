b82931a1c897b05fcc9cc62ebf2bcd47f072ec10
Glenn Van Hulle <quazasoft@gmail.com>
2023-02-08 14:18:58 +0100

    models project + person

---
+++ server/src/TickTick/TickTick.Models/base/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickTick.Models
+{
+    public abstract class BaseEntity
+    {
+        public ulong Id { get; set; }
+
+    }
+
+    public class BaseAuditableEntity : BaseEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
